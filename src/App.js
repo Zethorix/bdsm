34093e9ebc046c38226d7c73d516e95c3418f247
Zethorix <zethorixx@gmail.com>
2021-11-20 12:14:41 -0800

    Update header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Simulator from "./components/Simulator.js";
 function App() {
   return (
     <div>
-      <h1>Boxbox Dungeon Simulator</h1>
+      <h1>Boxbox Dungeon SiMulator (BDSM)</h1>
       <p>Enter your party's items and monuments.</p>
       <br />
       <Simulator />
