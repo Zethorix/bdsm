627bbecffb4209ec3f9915ded050c4ac6c2b431c
qaisklyi <silverleaf728@gmail.com>
2021-12-05 17:23:13 -0800

    UI indicator if player profile is active

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -5,36 +5,41 @@ import PlayerSummary from './PlayerSummary.js';
 import './PlayerForm.css';
 
 function PlayerForm(props) {
+  function maybeAddDisabledClassName(className) {
+    return className + (props.player.username == "" ? " disabled" : "")
+  }
+
   return (
     <div className="playerForm">
       <LoadPlayerSection
         player={props.player}
         onPlayerChanged={props.onPlayerChanged}
       />
-      <hr className="divider"></hr>
-      Username:
-      <input value={props.player.username} onChange={(event) => {
-        props.onPlayerChanged(event.target.value, props.player.items, props.player.monuments);
-      }} />
-      {props.player.items.map((item, index) =>
-        <ItemDropdown
-          key={index}
-          item={item}
-          onItemChanged={(name, tier) => {
-            let newItems = [...props.player.items];
-            newItems[index] = { name: name, tier: parseInt(tier) };
-            props.onPlayerChanged(props.player.username, newItems, props.player.monuments);
-          }}
+      <div className={maybeAddDisabledClassName("profile")}>
+        Username:
+        <input value={props.player.username} onChange={(event) => {
+          props.onPlayerChanged(event.target.value, props.player.items, props.player.monuments);
+        }} />
+        {props.player.items.map((item, index) =>
+          <ItemDropdown
+            key={index}
+            item={item}
+            onItemChanged={(name, tier) => {
+              let newItems = [...props.player.items];
+              newItems[index] = { name: name, tier: parseInt(tier) };
+              props.onPlayerChanged(props.player.username, newItems, props.player.monuments);
+            }}
+          />
+        )}
+        <MonumentsSection
+          monuments={props.player.monuments}
+          onMonumentsChanged={
+            (newMonuments) =>
+              props.onPlayerChanged(props.player.username, props.player.items, newMonuments)
+          }
         />
-      )}
-      <MonumentsSection
-        monuments={props.player.monuments}
-        onMonumentsChanged={
-          (newMonuments) =>
-            props.onPlayerChanged(props.player.username, props.player.items, newMonuments)
-        }
-      />
-      <PlayerSummary player={props.player}></PlayerSummary>
+        <PlayerSummary player={props.player}></PlayerSummary>
+      </div>
     </div>
   );
 }
